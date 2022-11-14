a1ba4f7180471b8deeb5ac13a62c44cdb0809978
fcu-d0845073 <loco95123@gmail.com>
2022-11-14 14:46:38 +0800

    change code in main

--- src/main/java/com/github/fcud0845073/Main.java
+++ src/main/java/com/github/fcud0845073/Main.java
@@ -2,6 +2,6 @@ package com.github.fcud0845073;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hi world!");
     }
 }
