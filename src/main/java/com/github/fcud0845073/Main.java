dd33a847c902847f64daca4d5314bdd88f60c570
fcu-d0845073 <loco95123@gmail.com>
2022-11-14 14:52:31 +0800

    add code in main

--- src/main/java/com/github/fcud0845073/Main.java
+++ src/main/java/com/github/fcud0845073/Main.java
@@ -3,5 +3,6 @@ package com.github.fcud0845073;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi world!");
+        System.out.println("C87");
     }
 }
